9b35f40f234fe718fef97577b11c43edfd72c547
Fredrik Sy <fredrik.sy@icloud.com>
2020-10-11 00:19:30 +0200

    Add StatusCode.cs

---
+++ StatusCode.cs
@@ -0,0 +1,12 @@
+namespace N1T1Tool
+{
+    enum StatusCode
+    {
+        Success,
+        DeviceNotFound,
+        DHCPConifgureFailed,
+        FileNotFound,
+        UpdateFailed,
+        TFTPFailed
+    }
+}
